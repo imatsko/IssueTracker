895bdaf0658df93023c74192b08130e164d930c5
Igor Matsko <rigelrisen@gmail.com>
2014-04-20 14:22:10 +0400

    Добавлены простейшие комментарии и их вывод в виде дерева.

--- models/issue_db.py
+++ models/issue_db.py
@@ -1,5 +1,15 @@
 # coding: utf8
 
+db.define_table('comment_post',
+    Field('prev_comment', 'reference comment_post', readable=False, writable=False),
+    Field('body', 'text'),
+    Field('created_on', 'datetime', default=request.now, readable=False, writable=False),
+    Field('created_by', 'reference auth_user', default=auth.user_id, readable=False, writable=False)
+    )
+
+db.comment_post.prev_comment.requires = IS_EMPTY_OR(IS_IN_DB(db, 'comment_post.id'))
+db.comment_post.body.requires = IS_NOT_EMPTY()
+
 
 db.define_table('issue',
     Field('title'),
@@ -8,12 +18,14 @@ db.define_table('issue',
 #     Field('created_by', 'reference auth_user', default=auth.user_id),
 #     Field('modified_on', 'datetime', default=request.now),
 #     Field('modified_by', 'reference auth_user', default=auth.user_id),
-#     Field('root_comment', 'reference comment'),
+    Field('root_comment', 'reference comment_post', readable=False, writable=False),
     format='%(title)s')
 
 db.issue.id.readable = False
 db.issue.title.requires = IS_NOT_IN_DB(db, 'issue.title')
 db.issue.body.requires = IS_NOT_EMPTY()
+db.issue.root_comment.requires = IS_IN_DB(db, 'comment_post.id')
+
 # db.issue.created_by.readable = db.issue.created_by.writable = False
 # db.issue.created_on.readable = db.issue.created_on.writable = False
 
@@ -33,15 +45,3 @@ db.attachment.issue_id.readable = db.attachment.issue_id.writable = False
 # db.document.created_by.readable = db.document.created_by.writable = False
 # db.document.created_on.readable = db.document.created_on.writable = False
 
-
-
-# db.define_table('comment',
-#     Field('prev_comment', 'reference comment'),
-#     Field('body', 'text'),
-#     Field('created_on', 'datetime', default=request.now),
-#     Field('created_by', 'reference auth_user', default=auth.user_id))
-
-# db.comment.body.requires = IS_NOT_EMPTY()
-# db.comment.prev_comment.readable = db.comment.prev_comment.writable = False
-# db.comment.created_by.readable = db.comment.created_by.writable = False
-# db.comment.created_on.readable = db.comment.created_on.writable = False
